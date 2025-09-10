78488e17bb5b0158e33685970054342e315d56f3
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-09-10 11:29:47 +0600

    Add table with Groups

---
+++ src/Client/NexusUserTest.Admin/Page/Home.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using NexusUserTest.Common.DTOs;
+using NexusUserTest.Shared;
+using NexusUserTest.Shared.NexusBlazor;
+using NexusUserTest.Shared.Services;
+
+namespace NexusUserTest.Admin.Page
+{
+    public partial class Home
+    {
+        [Inject]
+        public IAPIService? ServiceAPI { get; set; }
+        [Inject]
+        public INexusNotificationService? NotificationService { get; set; }
+        [Inject]
+        public INexusDialogService? DialogService { get; set; }
+
+        private NexusTableGrid<GroupDTO>? NexusTable;
+        private List<GroupDTO>? Items;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await LoadData();
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+
+        private async Task LoadData()
+        {
+            var g = await ServiceAPI!.GroupService.GetAllGroup("Specialization,GroupUser");
+            Items = [.. g];
+        }
+
+        private void DbClick(GroupDTO item)
+            => NotificationService!.ShowSuccess($"{item.Title}", "Успех");
+    }
+}
