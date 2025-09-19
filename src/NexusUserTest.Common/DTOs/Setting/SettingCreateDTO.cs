2dbaead3433ae2a69be6b7b22776400205426249
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-09-19 10:53:15 +0600

    Added new DTO for Setting and User

---
+++ src/NexusUserTest.Common/DTOs/Setting/SettingCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NexusUserTest.Common.DTOs
+{
+    public class SettingCreateDTO
+    {
+        public int GroupId { get; set; }
+        [Display(Name = "Количество вопросов")]
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
+        public int CountOfQuestion { get; set; }
+        [Display(Name = "Время на прохождение теста")]
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
+        public TimeSpan Timer { get; set; }
+    }
+}
---
+++ src/NexusUserTest.Common/DTOs/Setting/SettingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusUserTest.Common.DTOs
+{
+    public class SettingDTO
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public int CountOfQuestion { get; set; }
+        public TimeSpan Timer { get; set; }
+    }
+}
