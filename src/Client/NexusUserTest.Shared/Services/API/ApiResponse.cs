92295d4d00c1372274a2ee17f33cce5e9d0b70d9
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-09-30 14:01:45 +0600

    Added new class of Api response

---
+++ src/Client/NexusUserTest.Shared/Services/API/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace NexusUserTest.Shared.Services
+{
+    public class ApiResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Data { get; set; }
+        public string? Error { get; set; }
+
+        public static ApiResponse<T> Success(T? data) => new() { IsSuccess = true, Data = data };
+        public static ApiResponse<T> Errors(string error) => new() { IsSuccess = false, Error = error };
+    }
+}
