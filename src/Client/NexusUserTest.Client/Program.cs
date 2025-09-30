b32157a4833df036039bf173da2101549a4cc9c6
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-09-30 14:14:39 +0600

    Serilog was added

--- src/Client/NexusUserTest.Client/Program.cs
+++ src/Client/NexusUserTest.Client/Program.cs
@@ -1,5 +1,6 @@
 using NexusUserTest.Client.Components;
 using NexusUserTest.Shared.Services;
+using Serilog;
 
 namespace NexusUserTest.Client
 {
@@ -7,34 +8,58 @@ namespace NexusUserTest.Client
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            builder.Services.ConfigureHttpClient();
-            builder.Services.ConfigureAPI();
-            builder.Services.AddNexusBlazor();
-
-            var app = builder.Build();
-
-            if (!app.Environment.IsDevelopment())
+            try
             {
-                app.UseDeveloperExceptionPage();
-                app.UseHsts();
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+
+                Log.Information("Starting web application");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Services.AddRazorComponents()
+                    .AddInteractiveServerComponents();
+
+                builder.Services.AddSerilog();
+                builder.Services.ConfigureHttpClient();
+                builder.Services.ConfigureAPI();
+                builder.Services.AddNexusBlazor();
+
+                var app = builder.Build();
+
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseStaticFiles();
+                app.UseAntiforgery();
+
+                app.MapStaticAssets();
+                app.MapRazorComponents<App>()
+                    .AddAdditionalAssemblies([typeof(Admin._Imports).Assembly, typeof(User._Imports).Assembly])
+                    .AddInteractiveServerRenderMode();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapStaticAssets();
-            app.MapRazorComponents<App>()
-                .AddAdditionalAssemblies([typeof(Admin._Imports).Assembly, typeof(User._Imports).Assembly])
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
