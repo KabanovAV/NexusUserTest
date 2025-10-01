9305b0bf0747359fa15a91c1e7e25b5810d4f8e6
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-01 14:38:58 +0600

    Add new user page interface and countdown timer view

---
+++ src/Client/NexusUserTest.User/Pages/Home.razor.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.AspNetCore.Components;
+using NexusUserTest.Common;
+using NexusUserTest.Shared.NexusBlazor;
+using NexusUserTest.Shared.Services;
+using NexusUserTest.User.Views;
+
+namespace NexusUserTest.User.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        public IAPIService? ServiceAPI { get; set; }
+        [Inject]
+        public INexusNotificationService? NotificationService { get; set; }
+        [Inject]
+        public INexusDialogService? DialogService { get; set; }
+
+        private ApiResponse<UserInfoTestDTO>? UserInfo;
+        private GroupUserTestDTO? UserTest;
+        private List<QuestionTestDTO>? Questions;
+        private List<ResultTestDTO>? TestQuestions;
+
+        private Countdown Timer = default!;
+        private int SecondsRemain = 0;
+        private int CurrentQuestionIndex = 0;
+        private int TestProgressPercent => (int)((double)(TestQuestions.Count(t => t.AnswerId != null)) / TestQuestions!.Count * 100);
+        private bool IsTesting;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await LoadData();
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+
+        private async Task LoadData()
+            => UserInfo = await ServiceAPI!.UserService.GetUserTestInfo(1, "GroupUser.Group,GroupUser.Results.Answer");
+
+        private async void ButtonClick(int groupUserId)
+        {
+            UserTest = await ServiceAPI!.GroupUserService.GetTestGroupUser(groupUserId, "Group.Setting,Results.Question.Answers");
+            TestQuestions = [];
+
+            if (UserTest!.Results != null && UserTest!.Results.Count == 0)
+            {
+                SecondsRemain = (int)UserTest!.Timer.TotalSeconds;
+                UserTest.EndTest = DateTime.Now + UserTest!.Timer;
+                await ServiceAPI!.GroupUserService.UpdateTestGroupUser(UserTest);
+
+                Questions = await ServiceAPI!.TopicQuestionService.GetAllQuestionsBySpecializationId(UserTest.SpecializationId, "Topics.TopicQuestion.Question.Answers");
+                Shuffle(Questions);
+
+                foreach (var question in Questions.GetRange(0, UserTest.CountOfQuestion))
+                {
+                    Shuffle(question.AnswerItems!);
+                    var result = new ResultTestDTO
+                    {
+                        GroupUserId = groupUserId,
+                        Question = question,
+                        AnswerId = null
+                    };
+                    TestQuestions.Add(result);
+                }
+                TestQuestions = await ServiceAPI!.ResultService.AddRangeTestResultAsync(TestQuestions, "Question.Answers");
+            }
+            else
+            {
+                TimeSpan interval = UserTest!.EndTest - DateTime.Now;
+                SecondsRemain = (int)interval.TotalSeconds;
+                foreach (var result in UserTest.Results!)
+                    TestQuestions.Add(result);
+            }
+            IsTesting = true;
+            await InvokeAsync(StateHasChanged);
+            Timer.Start(SecondsRemain);
+        }
+
+        private void Shuffle<T>(List<T> list)
+        {
+            Random rng = new();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                (list[n], list[k]) = (list[k], list[n]);
+            }
+        }
+
+        private void TimerPauseCallback(int seconds) => SecondsRemain = seconds;
+
+        private async Task TimerOutCallback()
+        {
+            var settings = new NexusDialogSetting("Время истекло!", "Время на прохождение теста закончилось! Тест завершен", "Завершить");
+            await DialogService!.Show(settings);
+            await SaveResults();
+        }
+
+        private async Task SetAnswer(ResultTestDTO result, int? value)
+        {
+            if (result.AnswerId != value)
+            {
+                result.AnswerId = value;
+                await ServiceAPI!.ResultService.UpdateTestResult(result);
+            }
+        }
+
+        private void PrevQuestion()
+        {
+            if (CurrentQuestionIndex > 0)
+                CurrentQuestionIndex--;
+        }
+
+        private void NextQuestion()
+        {
+            if (CurrentQuestionIndex < TestQuestions!.Count - 1)
+                CurrentQuestionIndex++;
+        }
+
+        private void GoToQuestion(int index) => CurrentQuestionIndex = index;
+
+        private async Task CompleteTest()
+        {
+            if (TestProgressPercent != 100)
+            {
+                var settings = new NexusDialogSetting("Внимание!", "Не на все вопросы даны ответы. Вы уверены, что хотите завершить тест?", "Отменить", "Завершить");
+                var result = await DialogService!.Show(settings);
+                if (result?.Canceled == false)
+                    await SaveResults();
+            }
+            else if (SecondsRemain != 0)
+            {
+                var settings = new NexusDialogSetting("Внимание!", "Время на прохождение теста еще не истекло. Вы уверены, что хотите завершить тест?", "Отменить", "Завершить");
+                var result = await DialogService!.Show(settings);
+                if (result?.Canceled == false)
+                    await SaveResults();
+            }
+        }
+
+        private async Task SaveResults()
+        {
+            UserTest!.Status = 3;
+            await ServiceAPI!.GroupUserService.UpdateTestGroupUser(UserTest);
+
+            var group = UserInfo!.GroupUsers!.First(u => u.Id == UserTest.Id);
+            group.Status = 3;
+            group.Results = await ServiceAPI!.ResultService.GetAllTestInfoResultsync(UserTest.Id, "Answer");
+
+            UserTest = null;
+            IsTesting = false;
+            TestQuestions = null;
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+}
---
+++ src/Client/NexusUserTest.User/Views/Countdown.razor.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Timers;
+
+namespace NexusUserTest.User.Views
+{
+    public partial class Countdown : ComponentBase, IDisposable
+    {
+        [Parameter]
+        public EventCallback TimerOut { get; set; }
+        [Parameter]
+        public EventCallback<int> TimerPause { get; set; }
+
+        private System.Timers.Timer _timer = null!;
+        private int _secondsToRun = 0;
+        protected string Time { get; set; } = "00:00:00";
+
+        protected string Hours { get; set; } = "00";
+        protected string Minutes { get; set; } = "00";
+        protected string Seconds { get; set; } = "00";
+
+        protected override void OnInitialized()
+        {
+            _timer = new System.Timers.Timer(1000);
+            _timer.Elapsed += OnTimedEvent;
+            _timer.AutoReset = true;
+        }
+
+        private async void OnTimedEvent(object? sender, ElapsedEventArgs e)
+        {
+            await InvokeAsync(() =>
+            {
+                _secondsToRun--;
+                var timeSpan = TimeSpan.FromSeconds(_secondsToRun);
+                //Time = timeSpan.ToString(@"hh\:mm\:ss");
+
+                Hours = timeSpan.Hours.ToString("D2");
+                Minutes = timeSpan.Minutes.ToString("D2");
+                Seconds = timeSpan.Seconds.ToString("D2");
+
+                StateHasChanged();
+
+                if (_secondsToRun <= 0)
+                {
+                    _timer.Stop();
+                    TimerOut.InvokeAsync();
+                }
+            });            
+        }
+
+        public void Start(int secondsToRun)
+        {
+            _secondsToRun = secondsToRun;
+
+            if (_secondsToRun > 0)
+            {
+                var timeSpan = TimeSpan.FromSeconds(_secondsToRun);
+                //Time = timeSpan.ToString(@"hh\:mm\:ss");
+
+                Hours = timeSpan.Hours.ToString("D2");
+                Minutes = timeSpan.Minutes.ToString("D2");
+                Seconds = timeSpan.Seconds.ToString("D2");
+
+                StateHasChanged();
+                _timer.Start();
+            }
+        }
+
+        public async void Pause()
+        {
+            _timer.Stop();
+            await TimerPause.InvokeAsync(_secondsToRun);
+        }
+
+        public void Stop() => _timer.Stop();
+
+        public void Dispose() => _timer.Dispose();
+    }
+}
