9e148711fd90a429ebe1cddf4a0fda0fa64a62d4
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-02 15:48:21 +0600

    Методы возращают результат запроса и метод обновления не возвращает обьект

--- src/Client/NexusUserTest.Shared/Services/API/TopicAPIService.cs
+++ src/Client/NexusUserTest.Shared/Services/API/TopicAPIService.cs
@@ -1,99 +1,70 @@
-﻿using NexusUserTest.Common;
+﻿using Microsoft.AspNetCore.WebUtilities;
+using NexusUserTest.Common;
 using System.Net.Http.Json;
 
 namespace NexusUserTest.Shared.Services
 {
     public interface ITopicAPIService
     {
-        Task<List<TopicDTO>> GetAllTopic(string? include = null);
-        Task<List<SelectItem>> GetTopicSelect();
-        Task<TopicDTO?> GetTopic(int id, string? include = null);
-        Task<TopicDTO?> AddTopic(TopicDTO item, string? include = null);
-        Task<TopicDTO?> UpdateTopic(TopicDTO item, string? include = null);
-        Task DeleteTopic(int id);
+        Task<ApiResponse<List<TopicDTO>>> GetAllTopic(string? include = null);
+        Task<ApiResponse<TopicDTO>> GetTopic(int id, string? include = null);
+        Task<ApiResponse<List<SelectItem>>> GetTopicSelect();
+        Task<ApiResponse<TopicDTO>> AddTopic(TopicDTO item, string? include = null);
+        Task<ApiResponse<Unit>> UpdateTopic(TopicDTO item);
+        Task<ApiResponse<Unit>> DeleteTopic(int id);
     }
 
-    public class TopicAPIService(IHttpClientFactory httpClienFactory) : ITopicAPIService
+    public class TopicAPIService(IHttpClientFactory httpClienFactory, IApiResponseHandler responseHandler) : ITopicAPIService
     {
         private readonly HttpClient _httpClient = httpClienFactory.CreateClient("HttpClient");
+        private readonly IApiResponseHandler _responseHandler = responseHandler;
 
-        public async Task<List<TopicDTO>> GetAllTopic(string? include = null)
+        public async Task<ApiResponse<List<TopicDTO>>> GetAllTopic(string? include = null)
         {
-            try
+            if (include != null)
             {
-                var response = await _httpClient.GetAsync($"api/topics?include={include}");
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<List<TopicDTO>>() ?? [];
+                var url = QueryHelpers.AddQueryString("api/topics", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<List<TopicDTO>>(() =>
+                    _httpClient.GetAsync(url), "GetAllTopic");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return [];
-            }
+            return await _responseHandler.ExecuteHttpAsync<List<TopicDTO>>(() =>
+                    _httpClient.GetAsync("api/topics"), "GetAllTopic");
         }
 
-        public async Task<List<SelectItem>> GetTopicSelect()
+        public async Task<ApiResponse<TopicDTO>> GetTopic(int id, string? include = null)
         {
-            var s = await GetAllTopic();
-            return [.. s.Select(s => new SelectItem { Text = s.Title, Value = s.Id })];
+            if (include != null)
+            {
+                var url = QueryHelpers.AddQueryString($"api/topics/{id}", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<TopicDTO>(() =>
+                    _httpClient.GetAsync(url), "GetTopic");
+            }
+            return await _responseHandler.ExecuteHttpAsync<TopicDTO>(() =>
+                    _httpClient.GetAsync($"api/topics/{id}"), "GetTopic");
         }
 
-        public async Task<TopicDTO?> GetTopic(int id, string? include = null)
+        public async Task<ApiResponse<List<SelectItem>>> GetTopicSelect()
+        => await _responseHandler.ExecuteHttpAsync<List<SelectItem>>(() =>
+                _httpClient.GetAsync("api/topics/select"), "GetTopicSelect");
+
+        public async Task<ApiResponse<TopicDTO>> AddTopic(TopicDTO item, string? include = null)
         {
-            try
+            if (include != null)
             {
-                var response = await _httpClient.GetAsync($"api/topics/{id}?include={include}");
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<TopicDTO>();
+                var url = QueryHelpers.AddQueryString("api/topics", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<TopicDTO>(() =>
+                    _httpClient.PostAsJsonAsync(url, item), "AddTopic");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return null;
-            }
+            return await _responseHandler.ExecuteHttpAsync<TopicDTO>(() =>
+                    _httpClient.PostAsJsonAsync("api/topics", item), "AddTopic");
         }
 
-        public async Task<TopicDTO?> AddTopic(TopicDTO item, string? include = null)
-        {
-            try
-            {
-                using var response = await _httpClient.PostAsJsonAsync($"api/topics?include={include}", item);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<TopicDTO>();
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return null;
-            }
-        }
+        public async Task<ApiResponse<Unit>> UpdateTopic(TopicDTO item)
+            => await _responseHandler.ExecuteHttpAsync(() =>
+                    _httpClient.PutAsJsonAsync($"api/topics/{item.Id}", item), "UpdateTopic");
 
-        public async Task<TopicDTO?> UpdateTopic(TopicDTO item, string? include = null)
-        {
-            try
-            {
-                var response = await _httpClient.PutAsJsonAsync($"api/topics?include={include}", item);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<TopicDTO>();
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return null;
-            }
-        }
-
-        public async Task DeleteTopic(int id)
-        {
-            try
-            {
-                var response = await _httpClient.DeleteAsync($"api/topics/{id}");
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-            }
-        }
+        public async Task<ApiResponse<Unit>> DeleteTopic(int id)
+            => await _responseHandler.ExecuteHttpAsync(() =>
+                    _httpClient.DeleteAsync($"api/topics/{id}"), "DeleteTopic");
     }
 }
