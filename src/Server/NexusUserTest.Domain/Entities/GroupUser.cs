a8bcdbb25b6807cfa092015ac880a1451d5ed283
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-03 12:05:27 +0600

    Изменено свойство EndTest на nullable

--- src/Server/NexusUserTest.Domain/Entities/GroupUser.cs
+++ src/Server/NexusUserTest.Domain/Entities/GroupUser.cs
@@ -10,7 +10,7 @@ namespace NexusUserTest.Domain.Entities
         public int GroupId { get; set; }
         public int UserId { get; set; }
         public int Status { get; set; } // 1 Недопущен; 2 Допущен; 3 Пройден
-        public DateTime EndTest { get; set; }
+        public DateTime? EndTest { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ChangedDate { get; set; }
 
