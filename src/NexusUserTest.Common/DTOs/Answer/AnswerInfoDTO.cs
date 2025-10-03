e8a113c50160fc5959fe42c42ef55bf1aa83fbdf
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-03 10:14:19 +0600

    Классы перемешены в папку админ

--- src/NexusUserTest.Common/DTOs/Answer/AnswerInfoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NexusUserTest.Common
-{
-    public class AnswerInfoDTO
-    {
-        public int Id { get; set; }        
-        public string Title { get; set; } = string.Empty;
-        public bool IsCorrect { get; set; }
-    }
-}
