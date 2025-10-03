341480460ca950a3baefc418e0c1d146bec6c8b7
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-03 14:48:10 +0600

    У свойтсва TopicQuestionItems изменен типа с TopicQuestionCreateDTO на TopicQuestionDTO

--- src/NexusUserTest.Common/DTOs/Question/AdminDTO/QuestionAdminDTO.cs
+++ src/NexusUserTest.Common/DTOs/Question/AdminDTO/QuestionAdminDTO.cs
@@ -9,6 +9,6 @@ namespace NexusUserTest.Common
         [Required(ErrorMessage = "Обязательное поле для заполнения")]
         public string Title { get; set; } = string.Empty;
         public List<AnswerAdminDTO>? AnswerItems { get; set; }
-        public List<TopicQuestionCreateDTO>? TopicQuestionItems { get; set; }
+        public List<TopicQuestionDTO>? TopicQuestionItems { get; set; }
     }
 }
