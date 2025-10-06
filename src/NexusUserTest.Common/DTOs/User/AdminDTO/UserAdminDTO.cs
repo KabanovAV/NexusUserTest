5dd3d1b044cbdcb46e3f5f0ade52395bd8bef6d3
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-06 16:25:03 +0600

    Свойствам добавлен атрибут DisplayName

--- src/NexusUserTest.Common/DTOs/User/AdminDTO/UserAdminDTO.cs
+++ src/NexusUserTest.Common/DTOs/User/AdminDTO/UserAdminDTO.cs
@@ -1,12 +1,19 @@
-﻿namespace NexusUserTest.Common
+﻿using System.ComponentModel;
+
+namespace NexusUserTest.Common
 {
     public class UserAdminDTO
     {
         public int Id { get; set; }
+        [DisplayName("ФИО")]
         public string FullName { get; set; } = string.Empty;
+        [DisplayName("Логин")]
         public string Login { get; set; } = string.Empty;
+        [DisplayName("Пароль")]
         public string Password { get; set; } = string.Empty;
+        [DisplayName("Организация")]
         public string? Organization { get; set; }
+        [DisplayName("Позиция")]
         public string? Position { get; set; }
         public List<GroupUserAdminDTO>? GroupUserItems { get; set; }
     }
