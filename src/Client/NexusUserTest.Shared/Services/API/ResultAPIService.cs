3e3c89147316ac7fbe24f37f1e6204d5d6b6bdb1
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-06 11:01:18 +0600

    Переработаны методы и методы возвращают результат о состоянии запроса

--- src/Client/NexusUserTest.Shared/Services/API/ResultAPIService.cs
+++ src/Client/NexusUserTest.Shared/Services/API/ResultAPIService.cs
@@ -1,43 +1,42 @@
-﻿using NexusUserTest.Common;
+﻿using Microsoft.AspNetCore.WebUtilities;
+using NexusUserTest.Common;
 using System.Net.Http.Json;
 
 namespace NexusUserTest.Shared.Services
 {
     public interface IResultAPIService
     {
-        Task<List<ResultInfoAdminDTO>> GetAllInfoResultAsync(int groupUserId, string? include = null);
-        Task<List<ResultTestDTO>> GetAllTestResultAsync(int groupUserId, string? include = null);
-        Task<List<ResultInfoTestDTO>> GetAllTestInfoResultsync(int groupUserId, string? include = null);
-        Task<ResultTestDTO?> GetTestResultAsync(int id, string? include = null);
-        Task<ResultTestDTO?> AddTestResultAsync(ResultTestDTO entity, string? include = null);
-        Task<List<ResultTestDTO>?> AddRangeTestResultAsync(IEnumerable<ResultTestDTO> entities, string? include = null);
-        Task<ResultTestDTO?> UpdateTestResult(ResultTestDTO entity, string? include = null);
-        Task DeleteInfoResult(int groupUserId);
+        Task<ApiResponse<List<ResultInfoAdminDTO>>> GetAllInfoResultAsync(int groupUserId, string? include = null);
+        Task<ApiResponse<List<ResultInfoTestDTO>>> GetAllTestInfoResultAsync(int groupUserId, string? include = null);
+        Task<ApiResponse<List<ResultTestDTO>>> GetAllTestResultAsync(int groupUserId, string? include = null);
+        Task<ApiResponse<ResultTestDTO>> GetTestResultAsync(int id, string? include = null);
+        Task<ApiResponse<ResultTestDTO>> AddTestResultAsync(ResultTestDTO entity, string? include = null);
+        Task<ApiResponse<List<ResultTestDTO>>> AddRangeTestResultAsync(IEnumerable<ResultTestDTO> entities, string? include = null);
+        Task<ApiResponse<Unit>> UpdateTestResult(ResultTestDTO entity);
+        Task<ApiResponse<Unit>> DeleteInfoResult(int groupUserId);
     }
 
-    public class ResultAPIService(IHttpClientFactory httpClienFactory) : IResultAPIService
+    public class ResultAPIService(IHttpClientFactory httpClienFactory, IApiResponseHandler responseHandler) : IResultAPIService
     {
         private readonly HttpClient _httpClient = httpClienFactory.CreateClient("HttpClient");
+        private readonly IApiResponseHandler _responseHandler = responseHandler;
 
         /// <summary>
         /// Получение всех результатов теста пользователя группы
         /// </summary>
         /// <param name="groupUserId">Id группы пользователя</param>
         /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает список результатов ResultInfoDTO</returns>
-        public async Task<List<ResultInfoAdminDTO>> GetAllInfoResultAsync(int groupUserId, string? include = null)
+        /// <returns>Возвращает ответ состояния запроса со списком результатов ResultInfoDTO</returns>
+        public async Task<ApiResponse<List<ResultInfoAdminDTO>>> GetAllInfoResultAsync(int groupUserId, string? include = null)
         {
-            try
+            if (include != null)
             {
-                var response = await _httpClient.GetAsync($"api/results/groupuser/{groupUserId}/info?include={include}");
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<List<ResultInfoAdminDTO>>() ?? [];
+                var url = QueryHelpers.AddQueryString($"api/results/groupuser/{groupUserId}/info", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<List<ResultInfoAdminDTO>>(() =>
+                    _httpClient.GetAsync(url), "GetAllInfoResultAsync");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return [];
-            }
+            return await _responseHandler.ExecuteHttpAsync<List<ResultInfoAdminDTO>>(() =>
+                    _httpClient.GetAsync($"api/results/groupuser/{groupUserId}/info"), "GetAllInfoResultAsync");
         }
 
         /// <summary>
@@ -45,20 +44,17 @@ namespace NexusUserTest.Shared.Services
         /// </summary>
         /// <param name="groupUserId">Id группы пользователя</param>
         /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает список результатов ResultTestDTO</returns>
-        public async Task<List<ResultTestDTO>> GetAllTestResultAsync(int groupUserId, string? include = null)
+        /// <returns>Возвращает ответ состояния запроса со списком результатов ResultTestDTO</returns>
+        public async Task<ApiResponse<List<ResultInfoTestDTO>>> GetAllTestInfoResultAsync(int groupUserId, string? include = null)
         {
-            try
+            if (include != null)
             {
-                var response = await _httpClient.GetAsync($"api/results/groupuser/{groupUserId}/test?include={include}");
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<List<ResultTestDTO>>() ?? [];
+                var url = QueryHelpers.AddQueryString($"api/results/groupuser/{groupUserId}/test-info", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<List<ResultInfoTestDTO>>(() =>
+                    _httpClient.GetAsync(url), "GetAllTestInfoResultAsync");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return [];
-            }
+            return await _responseHandler.ExecuteHttpAsync<List<ResultInfoTestDTO>>(() =>
+                    _httpClient.GetAsync($"api/results/groupuser/{groupUserId}/test-info"), "GetAllTestInfoResultAsync");
         }
 
         /// <summary>
@@ -66,20 +62,17 @@ namespace NexusUserTest.Shared.Services
         /// </summary>
         /// <param name="groupUserId">Id группы пользователя</param>
         /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает список результатов ResultTestDTO</returns>
-        public async Task<List<ResultInfoTestDTO>> GetAllTestInfoResultsync(int groupUserId, string? include = null)
+        /// <returns>Возвращает ответ состояния запроса со списком результатов ResultTestDTO</returns>
+        public async Task<ApiResponse<List<ResultTestDTO>>> GetAllTestResultAsync(int groupUserId, string? include = null)
         {
-            try
+            if (include != null)
             {
-                var response = await _httpClient.GetAsync($"api/results/info/groupuser/{groupUserId}/test?include={include}");
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<List<ResultInfoTestDTO>>() ?? [];
+                var url = QueryHelpers.AddQueryString($"api/results/groupuser/{groupUserId}/test", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<List<ResultTestDTO>>(() =>
+                    _httpClient.GetAsync(url), "GetAllTestResultAsync");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return [];
-            }
+            return await _responseHandler.ExecuteHttpAsync<List<ResultTestDTO>>(() =>
+                    _httpClient.GetAsync($"api/results/groupuser/{groupUserId}/test"), "GetAllTestResultAsync");
         }
 
         /// <summary>
@@ -87,20 +80,17 @@ namespace NexusUserTest.Shared.Services
         /// </summary>
         /// <param name="id">Id результата</param>
         /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает результат ResultTestDTO</returns>
-        public async Task<ResultTestDTO?> GetTestResultAsync(int id, string? include = null)
+        /// <returns>Возвращает ответ состояния запроса с результатом ResultTestDTO</returns>
+        public async Task<ApiResponse<ResultTestDTO>> GetTestResultAsync(int id, string? include = null)
         {
-            try
+            if (include != null)
             {
-                var response = await _httpClient.GetAsync($"api/results/{id}/test?include={include}");
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<ResultTestDTO>();
+                var url = QueryHelpers.AddQueryString($"api/results/{id}/test", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<ResultTestDTO>(() =>
+                    _httpClient.GetAsync(url), "GetTestResultAsync");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return null;
-            }
+            return await _responseHandler.ExecuteHttpAsync<ResultTestDTO>(() =>
+                    _httpClient.GetAsync($"api/results/{id}/test"), "GetTestResultAsync");
         }
 
         /// <summary>
@@ -108,20 +98,17 @@ namespace NexusUserTest.Shared.Services
         /// </summary>
         /// <param name="entity">Результат</param>
         /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает результат ResultTestDTO</returns>
-        public async Task<ResultTestDTO?> AddTestResultAsync(ResultTestDTO entity, string? include = null)
+        /// <returns>Возвращает ответ состояния запроса с результатом ResultTestDTO</returns>
+        public async Task<ApiResponse<ResultTestDTO>> AddTestResultAsync(ResultTestDTO entity, string? include = null)
         {
-            try
+            if (include != null)
             {
-                using var response = await _httpClient.PostAsJsonAsync($"api/results/test?include={include}", entity);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<ResultTestDTO>();
+                var url = QueryHelpers.AddQueryString("api/results/test", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<ResultTestDTO>(() =>
+                    _httpClient.PostAsJsonAsync(url, entity), "AddTestResultAsync");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return null;
-            }
+            return await _responseHandler.ExecuteHttpAsync<ResultTestDTO>(() =>
+                    _httpClient.PostAsJsonAsync("api/results/test", entity), "AddTestResultAsync");
         }
 
         /// <summary>
@@ -129,58 +116,35 @@ namespace NexusUserTest.Shared.Services
         /// </summary>
         /// <param name="entities">Список результатов</param>
         /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает список результатов ResultTestDTO</returns>
-        public async Task<List<ResultTestDTO>?> AddRangeTestResultAsync(IEnumerable<ResultTestDTO> entities, string? include = null)
+        /// <returns>Возвращает ответ состояния запроса со списком результатов ResultTestDTO</returns>
+        public async Task<ApiResponse<List<ResultTestDTO>>> AddRangeTestResultAsync(IEnumerable<ResultTestDTO> entities, string? include = null)
         {
-            try
+            if (include != null)
             {
-                using var response = await _httpClient.PostAsJsonAsync($"api/results/test/batch?include={include}", entities);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<List<ResultTestDTO>>();
+                var url = QueryHelpers.AddQueryString("api/results/test/batch", "include", include);
+                return await _responseHandler.ExecuteHttpAsync<List<ResultTestDTO>>(() =>
+                    _httpClient.PostAsJsonAsync(url, entities), "AddRangeTestResultAsync");
             }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return [];
-            }
+            return await _responseHandler.ExecuteHttpAsync<List<ResultTestDTO>>(() =>
+                    _httpClient.PostAsJsonAsync("api/results/test/batch", entities), "AddRangeTestResultAsync");
         }
 
         /// <summary>
         /// Обновление результата в БД
         /// </summary>
         /// <param name="entity">Результат</param>
-        /// <param name="include">Подключение инклудов</param>
-        /// <returns>Возвращает результат ResultTestDTO</returns>
-        public async Task<ResultTestDTO?> UpdateTestResult(ResultTestDTO entity, string? include = null)
-        {
-            try
-            {
-                var response = await _httpClient.PutAsJsonAsync($"api/results/test?include={include}", entity);
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadFromJsonAsync<ResultTestDTO>();
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-                return null;
-            }
-        }
+        /// <returns>Возвращает ответ состояния запроса</returns>
+        public async Task<ApiResponse<Unit>> UpdateTestResult(ResultTestDTO entity)
+            => await _responseHandler.ExecuteHttpAsync(() =>
+                    _httpClient.PatchAsJsonAsync($"api/results/{entity.Id}/test", entity), "UpdateTestResult");
 
         /// <summary>
         /// Удаление результатов из БД
         /// </summary>
         /// <param name="groupUserId">Id группы пользователя</param>
-        public async Task DeleteInfoResult(int groupUserId)
-        {
-            try
-            {
-                var response = await _httpClient.DeleteAsync($"api/results/{groupUserId}");
-                response.EnsureSuccessStatusCode();
-            }
-            catch (HttpRequestException ex)
-            {
-                Console.WriteLine($"API Error: {ex.Message}");
-            }
-        }
+        /// /// <returns>Возвращает ответ состояния запроса</returns>
+        public async Task<ApiResponse<Unit>> DeleteInfoResult(int groupUserId)
+            => await _responseHandler.ExecuteHttpAsync(() =>
+                    _httpClient.DeleteAsync($"api/results/{groupUserId}"), "DeleteInfoResult");
     }
 }
