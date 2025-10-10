5ef3a5fd3a0d0f4e3061461cadb61a43381e9bfb
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-10 09:50:55 +0600

    Добавлены атрибуты DisplayName, Required и StringLength

--- src/NexusUserTest.Common/DTOs/User/AdminDTO/UserAdminDTO.cs
+++ src/NexusUserTest.Common/DTOs/User/AdminDTO/UserAdminDTO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace NexusUserTest.Common
 {
@@ -6,10 +7,14 @@ namespace NexusUserTest.Common
     {
         public int Id { get; set; }
         [DisplayName("ФИО")]
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
+        [StringLength(90, ErrorMessage = "Количество символов до 90")]
         public string FullName { get; set; } = string.Empty;
         [DisplayName("Логин")]
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
         public string Login { get; set; } = string.Empty;
         [DisplayName("Пароль")]
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
         public string Password { get; set; } = string.Empty;
         [DisplayName("Организация")]
         public string? Organization { get; set; }
