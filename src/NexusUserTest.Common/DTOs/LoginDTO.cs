6f3854c60eee6743049bd471d64e7a0662eba5b2
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-10 11:04:35 +0600

    Добавлен DTO для логирования пользователя

---
+++ src/NexusUserTest.Common/DTOs/LoginDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NexusUserTest.Common
+{
+    public class LoginDto()
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
+        [DisplayName("Логин")]
+        public string? UserLogin { get; set; }
+        [Required(ErrorMessage = "Обязательное поле для заполнения")]
+        [DisplayName("Пароль")]
+        public string? Password { get; set; }
+    }
+}
