ca39653c921244bf07540fef0ec6ec67610e2e66
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-10 11:19:50 +0600

    Добавлен новый компонен заполнения поля для пароля

---
+++ src/Client/NexusUserTest.Shared/NexusBlazor/NexusForm/NexusPassword.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace NexusUserTest.Shared.NexusBlazor
+{
+    public partial class NexusPassword
+    {
+        [Parameter]
+        public string? Placeholder { get; set; }
+        [Parameter]
+        public int MaxLength { get; set; } = 524288;
+
+        protected string CurrentValueAsString
+        {
+            get => Value!;
+            set => CurrentValue = value;
+        }
+
+        protected override void OnParametersSet()
+        {
+            // Добавляем placeholder если указан
+            if (!string.IsNullOrEmpty(Placeholder) && !AdditionalAttributes.ContainsKey("placeholder"))
+                AdditionalAttributes["placeholder"] = Placeholder;
+        }
+
+        private void HandleInput(string? inputValue)
+        {
+            if (!string.IsNullOrEmpty(inputValue))
+                CurrentValueAsString = inputValue;
+        }
+
+        private void HandleBlur(FocusEventArgs e)
+        {
+            // Форсируем валидацию при потере фокуса
+            if (EditContext != null && NexusValueExpression != null)
+                EditContext.NotifyFieldChanged(FieldIdentifier);
+            this.StateHasChanged();
+        }
+    }
+}
