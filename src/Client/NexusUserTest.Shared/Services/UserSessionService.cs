0515966b27558d5209b9525341f1f7977a7564cf
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-10 11:06:29 +0600

    Добавлен сервис для хранения информации подключенного пользователя

---
+++ src/Client/NexusUserTest.Shared/Services/UserSessionService.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace NexusUserTest.Shared.Services
+{
+    public class UserSessionService
+    {
+        public int UserId { get; private set; }
+        public string? UserName { get; private set; }
+
+        public void SetUser(ClaimsPrincipal user)
+        {
+            if (user.Identity is null || !user.Identity.IsAuthenticated)
+                return;
+
+            UserId = int.Parse(user.FindFirst("UserId")!.Value);
+            UserName = user.Identity.Name;
+        }
+    }
+}
