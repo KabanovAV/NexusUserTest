3bfec75001233dd3351673591c5115a5fd9934fb
Кабанов Алексей <AVK.Kabanov@gmail.com>
2025-10-10 11:11:40 +0600

    Добавлена кастомная реализация провайдера AuthenticationStateProvider

---
+++ src/Client/NexusUserTest.Shared/Services/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,57 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace NexusUserTest.Shared.Services
+{
+    public class CustomAuthenticationStateProvider(ILocalStorageService localStorage, IHttpClientFactory httpClienFactory) : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage = localStorage;
+        private readonly HttpClient _httpClient = httpClienFactory.CreateClient("HttpClient");
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+
+            if (string.IsNullOrWhiteSpace(token))
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+            // Настраиваем HTTP заголовок
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            // Распарсим JWT и создадим ClaimsPrincipal
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+
+            if (jwt.ValidTo < DateTime.UtcNow)
+            {
+                await _localStorage.RemoveItemAsync("authToken");
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+
+            var identity = new ClaimsIdentity(jwt.Claims, "jwt");
+            var user = new ClaimsPrincipal(identity);
+
+            return new AuthenticationState(user);
+        }
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+
+            var identity = new ClaimsIdentity(jwt.Claims, "jwt");
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void NotifyUserLogout()
+        {
+            var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+        }
+    }
+}
