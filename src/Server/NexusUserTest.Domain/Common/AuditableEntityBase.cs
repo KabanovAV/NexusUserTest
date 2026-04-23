a15e42e323fb57444ad38f55b21c39e3e2b50b3f
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-23 11:19:45 +0600

    Добавлена базовая сущность с одни свойством Id и сущность аудита с свойствами дата создания и редактирования

---
+++ src/Server/NexusUserTest.Domain/Common/AuditableEntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusUserTest.Domain.Common
+{
+    public class AuditableEntityBase : EntityBase
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime ChangedDate { get; set; }
+    }
+}
---
+++ src/Server/NexusUserTest.Domain/Common/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace NexusUserTest.Domain.Common
+{
+    public class EntityBase
+    {
+        public int Id { get; protected set; }
+    }
+}
