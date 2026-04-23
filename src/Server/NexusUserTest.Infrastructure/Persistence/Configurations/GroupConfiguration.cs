40ffc0487ba585422dbd0574d358bb80bbe58b2e
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-23 16:44:32 +0600

    Добавлены конфигурации сущностей базы для групп и вопросов

---
+++ src/Server/NexusUserTest.Infrastructure/Persistence/Configurations/GroupConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusUserTest.Domain.Entities;
+
+namespace NexusUserTest.Infrastructure
+{
+    public class GroupConfiguration : IEntityTypeConfiguration<Group>
+    {
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            builder.HasOne(s => s.Setting)
+                .WithOne(g => g.Group)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ src/Server/NexusUserTest.Infrastructure/Persistence/Configurations/QuestionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusUserTest.Domain.Entities;
+
+namespace NexusUserTest.Infrastructure
+{
+    public class QuestionConfiguration : IEntityTypeConfiguration<Question>
+    {
+        public void Configure(EntityTypeBuilder<Question> builder)
+        {
+            builder.HasMany(a => a.Answers)
+                .WithOne(q => q.Question)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
