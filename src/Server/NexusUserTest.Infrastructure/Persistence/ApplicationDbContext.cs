abe78148c2d263a2119bedb88c3813607e00328b
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-23 16:45:46 +0600

    Изменено название класса контекста базы данных

---
+++ src/Server/NexusUserTest.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NexusUserTest.Application.Common;
+using NexusUserTest.Domain.Common;
+using NexusUserTest.Domain.Entities;
+
+namespace NexusUserTest.Infrastructure
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Specialization> Specializations { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<GroupUser> GroupUsers { get; set; }
+        public DbSet<TopicQuestion> TopicQuestions { get; set; }
+        public DbSet<Result> Results { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(EntityBase).IsAssignableFrom(entityType.ClrType))
+                {
+                    modelBuilder.Entity(entityType.ClrType)
+                        .Property("Id")
+                        .ValueGeneratedOnAdd()
+                        .IsRequired();
+                }
+            }
+        }
+    }
+}
