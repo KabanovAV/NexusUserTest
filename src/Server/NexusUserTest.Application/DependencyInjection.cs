7a17cf3cb6873b6b4a6b07d6c09f4373bd02eca1
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-27 13:59:51 +0600

    Класс ApplicationServiceExtensions перееименован DependencyInjection и вынесен из папки в проект.

---
+++ src/Server/NexusUserTest.Application/DependencyInjection.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using NexusUserTest.Application.Common;
+using NexusUserTest.Application.Services;
+
+namespace NexusUserTest.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+            => services.ConfigurateCors().ConfigurateSwaggerGen().AddServices();
+
+        private static IServiceCollection ConfigurateCors(this IServiceCollection services)
+            => services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.WithOrigins("https://localhost:7113;http://localhost:5168")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
+        private static IServiceCollection ConfigurateSwaggerGen(this IServiceCollection services)
+            => services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("Version 1.0", new OpenApiInfo
+                {
+                    Version = "Version 1.0",
+                    Title = "Web API",
+                    Description = "Try repeat what I learn from DotNetTutorials"
+                });
+            });
+
+        private static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAnswerService, AnswerService>();
+            services.AddScoped<IGroupService, GroupService>();
+            services.AddScoped<IGroupUserService, GroupUserService>();
+            services.AddScoped<IQuestionService, QuestionService>();
+            services.AddScoped<IResultService, ResultService>();
+            services.AddScoped<ISettingService, SettingService>();
+            services.AddScoped<ISpecializationService, SpecializationService>();
+            services.AddScoped<ITopicQuestionService, TopicQuestionService>();
+            services.AddScoped<ITopicService, TopicService>();
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }            
+
+        //public static IServiceCollection ConfigurateAutoMapper(this IServiceCollection services, Assembly[] assembly)
+        //    => services.AddAutoMapper(assembly);
+
+        //public static IServiceCollection ConfigurateRepositoryService(this IServiceCollection services)
+        //    => services.AddScoped<IRepoServiceManager, RepoServiceManager>();
+    }
+}
