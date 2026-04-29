9432b5f6d86b2344eebd2f0ca2ef241cdfaf4e30
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-29 14:03:42 +0600

    Добавлен статистический класс вывода сообщения валидации

---
+++ src/Server/NexusUserTest.Application/Common/ValidationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace NexusUserTest.Application.Common
+{
+    public static class ValidationMessages
+    {
+        public const string Required = "Поле {0} обязательно для заполнения";
+        public const string Length = "Поле {0} должно быть от {1}";
+        public const string LengthFromTo = "Поле {0} должно быть от {1} до {2} символов";
+    }
+}
