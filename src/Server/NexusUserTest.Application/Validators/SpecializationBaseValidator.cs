0560d21f90484faff0fea69fa2508624289c429a
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-29 14:07:10 +0600

    Добавлена базовая валидация специализации

---
+++ src/Server/NexusUserTest.Application/Validators/SpecializationBaseValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using NexusUserTest.Application.Common;
+using NexusUserTest.Common;
+
+namespace NexusUserTest.Application.Validators
+{
+    public class SpecializationBaseValidator<T> : AbstractValidator<T> where T : SpecializationDTO
+    {
+        public SpecializationBaseValidator()
+        {
+            When(s => s.Title != null, () =>
+            {
+                RuleFor(s => s.Title)
+                    .NotEmpty().WithMessage(x => string.Format(ValidationMessages.Required, "Название"))
+                    .Length(100).WithMessage(x => string.Format(ValidationMessages.Length, "Название", 100));
+            });
+        }
+    }
+}
