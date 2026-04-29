2abda86c1380a21c25c8e44febb2c556863737f0
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-29 14:14:44 +0600

    Изменено название интерфейсов: IResultRepository на ITestResultRepository; ISettingRepository на ITestSettingRepository

--- src/Server/NexusUserTest.Domain/Common/Interfaces/Repositories/ITestSettingRepository.cs
+++ src/Server/NexusUserTest.Domain/Common/Interfaces/Repositories/ITestSettingRepository.cs
@@ -5,13 +5,13 @@ namespace NexusUserTest.Domain.Common
     /// <summary>
     /// Интерфейс с операциями для репозитория настройки теста
     /// </summary>
-    public interface ISettingRepository : IRepositoryOperations<Setting>
+    public interface ITestSettingRepository : IRepositoryOperations<TestSetting>
     {
         /// <summary>
         /// Получение настройки теста из набора данных
         /// </summary>
         /// <param name="id">Id настройки</param>
         /// <returns>Возвращает настройки теста из набора данных</returns>
-        Task<Setting?> GetSettingByIdAsync(int id);
+        Task<TestSetting?> GetSettingByIdAsync(int id);
     }
 }
