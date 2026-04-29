ad6ee7821b90aeb8fbb3d397d0a7bae99e22727d
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-29 15:00:07 +0600

    Класс перемещен из папки в начало проекта

---
+++ src/Server/NexusUserTest.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using NexusUserTest.Application.Common;
+using NexusUserTest.Domain.Common;
+using System.Text;
+
+namespace NexusUserTest.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+            => services.AddDatabase(configuration)
+                .AddRepository().AddAuthentication(configuration).AddAuthorization();
+
+        private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("PostgreConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+            services.AddScoped<IApplicationDbContext>(options => options.GetRequiredService<ApplicationDbContext>());
+            return services;
+        }
+
+        private static IServiceCollection AddRepository(this IServiceCollection services)
+            => services.AddScoped<IRepository, Repository>();
+
+        private static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings["Issuer"],
+                    ValidAudience = jwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+                };
+            });
+
+            return services;
+        }
+    }
+}
