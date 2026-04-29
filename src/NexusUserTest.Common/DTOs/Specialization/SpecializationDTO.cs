9d3989e1f5fc29a1f512ab3efe518c766ad05cc3
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-29 11:54:05 +0600

    Переделан класс DTO в запись

--- src/NexusUserTest.Common/DTOs/Specialization/SpecializationDTO.cs
+++ src/NexusUserTest.Common/DTOs/Specialization/SpecializationDTO.cs
@@ -1,13 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace NexusUserTest.Common
+﻿namespace NexusUserTest.Common
 {
-    public class SpecializationDTO
-    {
-        public int Id { get; set; }
-        [Display(Name = "Название")]
-        [Required(ErrorMessage = "Обязательное поле для заполнения")]
-        [StringLength(100, ErrorMessage = "Количество символов до 100")]
-        public string Title { get; set; } = string.Empty;
-    }
+    public record SpecializationDTO(int Id, string Title);
+
+    //public class SpecializationDTO
+    //{
+    //    public int Id { get; set; }
+    //    [Display(Name = "Название")]
+    //    [Required(ErrorMessage = "Обязательное поле для заполнения")]
+    //    [StringLength(100, ErrorMessage = "Количество символов до 100")]
+    //    public string Title { get; set; } = string.Empty;
+    //}
 }
