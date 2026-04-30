f1639d378487304430324eceb63392865ee82ab4
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-30 12:09:20 +0600

    Убраны атрибуты анотации

--- src/Server/NexusUserTest.Domain/Entities/Group.cs
+++ src/Server/NexusUserTest.Domain/Entities/Group.cs
@@ -1,25 +1,15 @@
 ﻿using NexusUserTest.Domain.Common;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace NexusUserTest.Domain.Entities
 {
     public class Group : AuditableEntityBase
     {
-        [Required, StringLength(200)]
-        public string Title { get; set; } = string.Empty;
-        [Required]
+        public string Title { get; set; } = null!;
         public int SpecializationId { get; set; }
-        [Required]
         public DateTime Begin { get; set; }
-        [Required]
         public DateTime End { get; set; }
-
-        [JsonIgnore]
         public Specialization? Specialization { get; set; }
-        [JsonIgnore]
         public List<GroupUser>? GroupUser { get; set; }
-        [JsonIgnore]
         public TestSetting? Setting { get; set; }
     }
 }
