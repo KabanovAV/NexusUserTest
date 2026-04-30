85ceef32ec60135e0f1b1a0ee86a723b3ab5da2d
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-30 15:30:44 +0600

    Переименованы методы с соответвием стандарту REST

--- src/Server/NexusUserTest.WebApi/Controllers/SpecializationsController.cs
+++ src/Server/NexusUserTest.WebApi/Controllers/SpecializationsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NexusUserTest.Application.Common;
 using NexusUserTest.Common;
+using NexusUserTest.Common.DTOs;
 using NexusUserTest.WebApi.Controllers;
 
 namespace SibCCSPETest.WebApi.Controllers
@@ -16,7 +17,7 @@ namespace SibCCSPETest.WebApi.Controllers
         /// <response code="200">Успешное выполнение запроса</response>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<SpecializationDTO>>> GetAllSpecialization()
+        public async Task<ActionResult<IEnumerable<SpecializationDTO>>> GetAll()
         {
             var specializations = await service.GetAllSpecializationAsync();
             return HandleOkResult(specializations);
@@ -32,7 +33,7 @@ namespace SibCCSPETest.WebApi.Controllers
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<SpecializationDTO>> GetSpecialization([FromRoute] int id)
+        public async Task<ActionResult<SpecializationDTO>> GetById([FromRoute] int id)
         {
             var specialization = await service.GetSpecializationByIdAsync(id);
             return HandleOkResult(specialization);
@@ -61,10 +62,10 @@ namespace SibCCSPETest.WebApi.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<SpecializationDTO>> AddSpecialization([FromBody] SpecializationDTO cSpecialization)
+        public async Task<ActionResult<SpecializationDTO>> Create([FromBody] CreateSpecializationDTO cSpecialization)
         {
-            var result = await service.AddSpecializationAsync(cSpecialization);
-            return HandleCreatedResult(result, nameof(GetSpecialization), new { id = result.Value.Id });
+            var result = await service.CreateSpecializationAsync(cSpecialization);
+            return HandleCreatedResult(result, nameof(GetById), new { id = result.Value.Id });
         }
 
         /// <summary>
@@ -79,7 +80,7 @@ namespace SibCCSPETest.WebApi.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> UpdateSpecialization([FromRoute] int id, [FromBody] SpecializationDTO uSpecialization)
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateSpecializationDTO uSpecialization)
         {
             var result = await service.UpdateSpecializationAsync(id, uSpecialization);
             return HandleNoContentResult(result);
@@ -94,7 +95,7 @@ namespace SibCCSPETest.WebApi.Controllers
         [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<bool>> DeleteSpecialization([FromRoute] int id)
+        public async Task<ActionResult<bool>> Delete([FromRoute] int id)
         {
             var result = await service.DeleteSpecializationAsync(id);
             return HandleOkResult(result);
