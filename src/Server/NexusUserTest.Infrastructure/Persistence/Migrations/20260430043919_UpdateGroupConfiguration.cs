8e8bcd86f8108a197e435c22e364117c399d2a09
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-04-30 12:08:26 +0600

    Изменено поведение удаления связей у специализации

---
+++ src/Server/NexusUserTest.Infrastructure/Persistence/Migrations/20260430043919_UpdateGroupConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NexusUserTest.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGroupConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Specializations_SpecializationId",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topics_Specializations_SpecializationId",
+                table: "Topics");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Specializations_SpecializationId",
+                table: "Groups",
+                column: "SpecializationId",
+                principalTable: "Specializations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topics_Specializations_SpecializationId",
+                table: "Topics",
+                column: "SpecializationId",
+                principalTable: "Specializations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Specializations_SpecializationId",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topics_Specializations_SpecializationId",
+                table: "Topics");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Specializations_SpecializationId",
+                table: "Groups",
+                column: "SpecializationId",
+                principalTable: "Specializations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topics_Specializations_SpecializationId",
+                table: "Topics",
+                column: "SpecializationId",
+                principalTable: "Specializations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
