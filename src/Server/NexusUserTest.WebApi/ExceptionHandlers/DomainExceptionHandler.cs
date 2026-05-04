26fa79e67c4c677c048ec87415cef0fed40534de
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-04 15:11:02 +0600

    Добавлен обработчик ошибок для домена

---
+++ src/Server/NexusUserTest.WebApi/ExceptionHandlers/DomainExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace NexusUserTest.WebApi
+{
+    internal sealed class DomainExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            Log.Warning(exception, "Ошибка домена: {Path} метод {Method}", httpContext.Request.Path, httpContext.Request.Method);
+
+            var problemDatails = new ProblemDetails
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Title = "Ошибка бизнес-логики",
+                Detail = exception.Message,
+                Instance = httpContext.Request.Path
+            };
+
+            httpContext.Response.StatusCode = problemDatails.Status.Value;
+            await httpContext.Response.WriteAsJsonAsync(problemDatails, cancellationToken);
+
+            return true;
+        }
+    }
+}
