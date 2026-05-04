58207e913f445ad1120bb9ea5f7158535b680139
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-04 15:00:30 +0600

    Добавлен обработчик глобальных ошибок

---
+++ src/Server/NexusUserTest.WebApi/GlobalExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace NexusUserTest.WebApi
+{
+    internal sealed class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            Log.Error(exception, "Необработанная ошибка: {Path} метод {Method}", httpContext.Request.Path, httpContext.Request.Method);
+
+            var problemDatails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Внутренняя ошибка сервера",
+                Detail = "Произошла внутренняя ошибка. Пожалуйста, попробуйте позже.",
+                Instance = httpContext.Request.Path
+            };
+
+            httpContext.Response.StatusCode = problemDatails.Status.Value;
+            await httpContext.Response.WriteAsJsonAsync(problemDatails, cancellationToken);
+
+            return true;
+        }
+    }
+}
