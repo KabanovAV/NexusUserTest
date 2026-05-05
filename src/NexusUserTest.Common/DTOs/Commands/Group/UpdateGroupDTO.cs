7f416df2386302d8f78ddd45dcb43d6dfc8b67ca
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 11:13:47 +0600

    Добавлена DTO для обновления группы

---
+++ src/NexusUserTest.Common/DTOs/Commands/Group/UpdateGroupDTO.cs
@@ -0,0 +1,5 @@
+﻿namespace NexusUserTest.Common.DTOs
+{
+    public record UpdateGroupDTO(int Id, string Title, int SpecializationId, DateTime Begin, DateTime End)
+        : GroupBaseDTO(Title, Begin, End);
+}
