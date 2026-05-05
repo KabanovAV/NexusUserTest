02201bf17ef1576bb6653f24aa9eac9662f96eae
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 14:21:45 +0600

    Добавлен общий класс вывода сообщений ошибок

---
+++ src/Server/NexusUserTest.Application/Common/QueryResult/Errors/GroupErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace NexusUserTest.Application.Common
+{
+    public static class GroupErrors
+    {
+        public static Error NotFound(int id)
+            => Error.NotFound("Group.NotFound", $"Группа с идентификатором '{id}' не найдена");
+
+        public static Error Conflict(int entityId, int dtoId)
+            => Error.Conflict("Group.NotEqualId", $"Несовпадение идентификаторов: ожидался '{entityId}', получен '{dtoId}'");
+    }
+}
