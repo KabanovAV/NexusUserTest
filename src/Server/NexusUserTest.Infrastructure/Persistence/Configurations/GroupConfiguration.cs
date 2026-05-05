570f77648b0a348ef344f295f910450b2019343a
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 14:32:43 +0600

    Добавлены конфигурацци свойств сущности группы

--- src/Server/NexusUserTest.Infrastructure/Persistence/Configurations/GroupConfiguration.cs
+++ src/Server/NexusUserTest.Infrastructure/Persistence/Configurations/GroupConfiguration.cs
@@ -8,6 +8,21 @@ namespace NexusUserTest.Infrastructure
     {
         public void Configure(EntityTypeBuilder<Group> builder)
         {
+            builder.Property(g => g.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(g => g.Begin)
+                .IsRequired();
+
+            builder.Property(g => g.End)
+                .IsRequired();
+
+            builder.HasMany(g => g.GroupUser)
+                .WithOne(gu => gu.Group)
+                .HasForeignKey(gu => gu.GroupId)
+                .OnDelete(DeleteBehavior.Restrict);
+
             builder.HasOne(s => s.Setting)
                 .WithOne(g => g.Group)
                 .OnDelete(DeleteBehavior.Cascade);
