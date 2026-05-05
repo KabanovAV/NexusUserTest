ca21296d89c53b77c0a4e68401a49c15ff8a2c96
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 14:18:01 +0600

    Внесены правки в интерфейс: методы возвращают результат запроса и переименованы параметры методов добавления и обновления

--- src/Server/NexusUserTest.Application/Common/Interfaces/IGroupService.cs
+++ src/Server/NexusUserTest.Application/Common/Interfaces/IGroupService.cs
@@ -1,4 +1,5 @@
-﻿using NexusUserTest.Domain.Entities;
+﻿using NexusUserTest.Common;
+using NexusUserTest.Common.DTOs;
 
 namespace NexusUserTest.Application.Common
 {
@@ -11,32 +12,39 @@ namespace NexusUserTest.Application.Common
         /// Получение всех групп из набора данных
         /// </summary>
         /// <returns>Возвращает список групп из набора данных</returns>
-        Task<IEnumerable<Group>> GetAllGroupAsync();
+        Task<Result<IEnumerable<GroupDTO>>> GetAllGroupAsync();
 
         /// <summary>
         /// Получение группы из набора данных по Id
         /// </summary>
         /// <param name="id">Id группы</param>
         /// <returns>Возвращает группу из набора данных</returns>
-        Task<Group?> GetGroupByIdAsync(int id);
+        Task<Result<GroupDTO>> GetGroupByIdAsync(int id);
+
+        /// <summary>
+        /// Получение выпадающего списка групп из набора данных
+        /// </summary>
+        /// <returns>Возвращает список групп из набора данных</returns>
+        Task<Result<IEnumerable<SelectItem>>> GetSelectGroupAsync();
 
         /// <summary>
         /// Добавить группу в набор данных
         /// </summary>
-        /// <param name="entity">Группа</param>
+        /// <param name="createDto">Группа</param>
         /// <returns>Возвращает группу после добавления в БД</returns>
-        Task<Group> AddGroupAsync(Group entity);
+        Task<Result<GroupDTO>> CreateGroupAsync(CreateGroupDTO createDto);
 
         /// <summary>
         /// Изменить группу в наборе данных
         /// </summary>
-        /// <param name="entity">Группа</param>
-        Task<Group> UpdateGroupAsync(Group entity);
+        /// <param name="id">Id группы</param>
+        /// <param name="updateDto">Группа</param>
+        Task<Result> UpdateGroupAsync(int id, UpdateGroupDTO updateDto);
 
         /// <summary>
         /// Удалить группу из набора данных
         /// </summary>
         /// <param name="id">Id группы</param>
-        Task DeleteGroupAsync(int id);
+        Task<Result<bool>> DeleteGroupAsync(int id);
     }
 }
