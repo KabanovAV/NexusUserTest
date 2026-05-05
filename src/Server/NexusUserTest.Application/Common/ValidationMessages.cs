478d0a9334235dbde0090ddbf3808835ff3bacc5
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 14:27:17 +0600

    Добавлен общий класс вывода сообщений ошибок

--- src/Server/NexusUserTest.Application/Common/ValidationMessages.cs
+++ src/Server/NexusUserTest.Application/Common/ValidationMessages.cs
@@ -5,5 +5,6 @@
         public const string Required = "Поле {0} обязательно для заполнения";
         public const string Length = "Поле {0} должно быть от {1}";
         public const string LengthFromTo = "Поле {0} должно быть от {1} до {2} символов";
+        public const string GreaterThan = "Значение {0} должно быть больше {1}";
     }
 }
