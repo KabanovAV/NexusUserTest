c7038e9c5e5d1d59b3ed2d8038c13ee42ec18f98
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 10:09:28 +0600

    Убран метод обновления сущности. Обновление сущности проихлдит через маппинг

--- src/Server/NexusUserTest.Domain/Entities/Specialization.cs
+++ src/Server/NexusUserTest.Domain/Entities/Specialization.cs
@@ -7,16 +7,5 @@ namespace NexusUserTest.Domain.Entities
         public string Title { get; set; } = null!;
         public List<Group>? Groups { get; set; }
         public List<Topic>? Topics { get; set; }
-
-        public bool ApplyUpdate(string? title)
-        {
-            var hasChanges = false;
-            if (title != null && !Title.Equals(title.Trim(), StringComparison.OrdinalIgnoreCase))
-            {
-                Title = title;
-                hasChanges = true;
-            }            
-            return hasChanges;
-        }
     }
 }
