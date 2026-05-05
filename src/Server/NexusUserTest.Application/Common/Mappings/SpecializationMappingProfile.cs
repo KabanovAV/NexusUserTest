1c570783a46b87838f27d6941da24dc2ca4ea068
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 10:10:07 +0600

    Добавлен маппинг обновления UpdateFromDto

---
+++ src/Server/NexusUserTest.Application/Common/Mappings/SpecializationMappingProfile.cs
@@ -0,0 +1,53 @@
+﻿using NexusUserTest.Common;
+using NexusUserTest.Common.DTOs;
+using NexusUserTest.Domain.Entities;
+
+namespace NexusUserTest.Application.Mappings
+{
+    public static class SpecializationMappingProfile
+    {
+        /// <summary>
+        /// Маппинг из обьекта Specialization в SpecializationDTO
+        /// </summary>
+        /// <param name="entity">Обьект Specialization</param>
+        /// <returns>SpecializationDTO</returns>
+        public static SpecializationDTO ToDto(this Specialization entity)
+            => new(entity.Id, entity.Title);
+
+        /// <summary>
+        /// Маппинг списка из обьектов Specialization в список SpecializationDTO
+        /// </summary>
+        /// <param name="entities">Список обьектов Specialization</param>
+        /// <returns>Список SpecializationDTO</returns>
+        public static List<SpecializationDTO> ToDto(this IEnumerable<Specialization> entities)
+            => [.. entities.Where(e => e != null).Select(e => e.ToDto())];
+
+        /// <summary>
+        /// Маппинг из обьекта Specialization в SelectItem
+        /// </summary>
+        /// <param name="entity">Обьект Specialization</param>
+        /// <returns>SelectItem</returns>
+        public static SelectItem ToSelect(this Specialization entity)
+            => new(entity.Id, entity.Title);
+
+        /// <summary>
+        /// Маппинг списка из обьектов Specialization в список SelectItem
+        /// </summary>
+        /// <param name="entities">Список обьектов Specialization</param>
+        /// <returns>Список SelectItem</returns>
+        public static List<SelectItem> ToSelect(this IEnumerable<Specialization> entities)
+            => [.. entities.Where(e => e != null).Select(e => e.ToSelect())];
+
+        /// <summary>
+        /// Маппинг обновления обьекта Specialization
+        /// </summary>
+        /// <param name="entity">Обьект Specialization</param>
+        /// <param name="dto">SpecializationDTO</param>
+        public static void UpdateFromDto(this Specialization entity, UpdateSpecializationDTO dto)
+        {
+            if (dto == null) return;
+            if (!string.IsNullOrWhiteSpace(dto.Title) && !entity.Title.Equals(dto.Title.Trim(), StringComparison.OrdinalIgnoreCase))
+                entity.Title = dto.Title;
+        }
+    }
+}
