84edec8972ed29c85ab355de2e8837d5ed5028f5
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-05 11:13:18 +0600

    Добавлена DTO для запросов группы

---
+++ src/NexusUserTest.Common/DTOs/Queries/GroupDTO.cs
@@ -0,0 +1,5 @@
+﻿namespace NexusUserTest.Common.DTOs
+{
+    public record GroupDTO(int Id, string Title, string SpecializationTitle, int CountUsers, DateTime Begin, DateTime End)
+        : GroupBaseDTO(Title, Begin, End);
+}
