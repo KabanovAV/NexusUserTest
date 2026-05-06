51889be348ce4f5d1aa800de19f0ed1cf70bce41
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-06 15:13:32 +0600

    Добавлен базовый DTO пользователя

---
+++ src/NexusUserTest.Common/DTOs/Bases/UserBaseDTO.cs
@@ -0,0 +1,4 @@
+﻿namespace NexusUserTest.Common.DTOs
+{
+    public record UserBaseDTO(string Lastname, string Firstname, string? Surname, string Login, string Password, string? Organization, string? Position);
+}
