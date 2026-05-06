20c37d674646d5da16b08a922dbc74e642974839
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-06 15:29:15 +0600

    Из сущности убраны атрибуты аннотации свойствам

--- src/Server/NexusUserTest.Domain/Entities/User.cs
+++ src/Server/NexusUserTest.Domain/Entities/User.cs
@@ -1,24 +1,16 @@
 ﻿using NexusUserTest.Domain.Common;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace NexusUserTest.Domain.Entities
 {
     public class User : AuditableEntityBase
     {
-        [Required, StringLength(30)]
-        public string LastName { get; set; } = string.Empty;
-        [Required, StringLength(30)]
-        public string FirstName { get; set; } = string.Empty;
+        public string Lastname { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
         public string? Surname { get; set; }
-        [Required]
-        public string Login { get; set; } = string.Empty;
-        [Required]
-        public string Password { get; set; } = string.Empty;
+        public string Login { get; set; } = null!;
+        public string Password { get; set; } = null!;
         public string? Organization { get; set; }
         public string? Position { get; set; }
-
-        [JsonIgnore]
-        public List<GroupUser>? GroupUser { get; set; }
+        public List<GroupUser>? GroupUsers { get; set; }
     }
 }
