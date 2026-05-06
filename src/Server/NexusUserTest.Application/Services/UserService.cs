bb9895af00d8a72960e85bd01780c5087e7dd32e
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-06 15:21:33 +0600

    Полностью переработан сервис пользователя с соответствии с интерфейсом сервиса

--- src/Server/NexusUserTest.Application/Services/UserService.cs
+++ src/Server/NexusUserTest.Application/Services/UserService.cs
@@ -1,6 +1,9 @@
 ﻿using NexusUserTest.Application.Common;
+using NexusUserTest.Application.Common.Errors;
+using NexusUserTest.Common.DTOs;
 using NexusUserTest.Domain.Common;
 using NexusUserTest.Domain.Entities;
+using SibCCSPETest.WebApi.MappingProfiles;
 
 namespace NexusUserTest.Application.Services
 {
@@ -10,53 +13,97 @@ namespace NexusUserTest.Application.Services
     public class UserService : IUserService
     {
         private readonly IRepository _repository;
+        private readonly IValidationService _validationService;
 
-        public UserService(IRepository repository)
+        public UserService(IRepository repository, IValidationService validationService)
         {
             _repository = repository;
+            _validationService = validationService;
         }
 
         /// <summary>
         /// Получение всех пользователей из набора данных
         /// </summary>
         /// <returns>Возвращает список пользователей из набора данных</returns>
-        public async Task<IEnumerable<User>> GetAllUserAsync()
-            => await _repository.User.GetAllUserAsync();
+        public async Task<Result<IEnumerable<UserDTO>>> GetAllUserAsync()
+        {
+            var users = await _repository.User.GetAllUserAsync();
+            return users.ToDto();
+        }
 
         /// <summary>
         /// Получение пользователя из набора данных по Id
         /// </summary>
         /// <param name="id">Id пользователя</param>
         /// <returns>Возвращает пользователя из набора данных</returns>
-        public async Task<User?> GetUserByIdAsync(int id)
-            => await _repository.User.GetUserByIdAsync(id);
+        public async Task<Result<UserDTO>> GetUserByIdAsync(int id)
+        {
+            var user = await _repository.User.GetUserByIdAsync(id);
+            if (user == null)
+                return Result.Failure<UserDTO>(UserErrors.NotFound(id));
+            return user.ToDto();
+        }
 
         /// <summary>
         /// Добавить пользователя в набор данных
         /// </summary>
-        /// <param name="entity">Пользователь</param>
+        /// <param name="createUser">Пользователь</param>
         /// <returns>Возвращает пользователя после добавления в БД</returns>
-        public async Task<User> AddUserAsync(User entity)
+        public async Task<Result<UserDTO>> CreateUserAsync(CreateUserDTO createDto)
         {
-            await _repository.User.AddAsync(entity);
-            return await _repository.User.GetUserByIdAsync(entity.Id);
+            var validation = await _validationService.ValidateAsync(createDto);
+            if (validation.IsSuccess)
+            {
+                var user = new User()
+                {
+                    Lastname = createDto.Lastname,
+                    Firstname = createDto.Firstname,
+                    Surname = createDto.Surname,
+                    Login = createDto.Login,
+                    Password = createDto.Password,
+                    Organization = createDto.Organization,
+                    Position = createDto.Position
+                };
+                await _repository.User.AddAsync(user);
+                return user.ToDto();
+            }
+            return Result.Failure<UserDTO>(validation.Error);
         }
 
         /// <summary>
         /// Изменить пользователя в наборе данных
         /// </summary>
-        /// <param name="entity">Пользователь</param>
-        public async Task<User> UpdateUserAsync(User entity)
+        /// <param name="id">Id пользователя</param>
+        /// <param name="updateDto">Пользователь</param>
+        public async Task<Result> UpdateUserAsync(int id, UpdateUserDTO updateDto)
         {
-            await _repository.User.Update(entity);
-            return await _repository.User.GetUserByIdAsync(entity.Id);
+            if (id != updateDto.Id)
+                return Result.Failure(UserErrors.Conflict(id, updateDto.Id));
+
+            var validation = await _validationService.ValidateAsync(updateDto);
+            if (validation.IsSuccess)
+            {
+                var user = await _repository.User.GetUserByIdAsync(id);
+                if (user == null)
+                    return Result.Failure(UserErrors.NotFound(id));
+                user.UpdateFromDto(updateDto);
+                await _repository.User.Update(user);
+                return Result.Success();
+            }
+            return Result.Failure(validation.Error);
         }
 
         /// <summary>
         /// Удалить пользователя из набора данных
         /// </summary>
         /// <param name="id">Id пользователя</param>
-        public async Task DeleteUserAsync(int id)
-            => await _repository.User.Remove(id);
+        public async Task<Result> DeleteUserAsync(int id)
+        {
+            var user = await _repository.User.GetUserByIdAsync(id);
+            if (user == null)
+                return Result.Failure(UserErrors.NotFound(id));
+            await _repository.User.Remove(id);
+            return Result.Success();
+        }
     }
 }
