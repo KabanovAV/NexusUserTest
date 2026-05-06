c657398250822ed47788753b2b1f640ce832fbe7
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-06 15:30:41 +0600

    Добавлена миграция обновляющая таблицу пользователя

---
+++ src/Server/NexusUserTest.Infrastructure/Persistence/Migrations/20260506055357_UpdateUserTable.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NexusUserTest.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupUsers_Groups_GroupId",
+                table: "GroupUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "Lastname");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Users",
+                newName: "Firstname");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Lastname",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "Users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupUsers_Groups_GroupId",
+                table: "GroupUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GroupUsers_Groups_GroupId",
+                table: "GroupUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Lastname",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Firstname",
+                table: "Users",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Users",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Users",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GroupUsers_Groups_GroupId",
+                table: "GroupUsers",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
