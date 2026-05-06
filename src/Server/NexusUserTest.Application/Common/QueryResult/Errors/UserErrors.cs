9f7094e0a1e31f5cf209d66297549fe4323a9455
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-06 15:17:29 +0600

    Добавлен общий класс сообщений об ошибках

---
+++ src/Server/NexusUserTest.Application/Common/QueryResult/Errors/UserErrors.cs
@@ -0,0 +1,11 @@
+﻿namespace NexusUserTest.Application.Common.Errors
+{
+    public static class UserErrors
+    {
+        public static Error NotFound(int id)
+            => Error.NotFound("User.NotFound", $"Пользователь с идентификатором '{id}' не найдена");
+
+        public static Error Conflict(int entityId, int dtoId)
+            => Error.Conflict("User.NotEqualId", $"Несовпадение идентификаторов: ожидался '{entityId}', получен '{dtoId}'");
+    }
+}
