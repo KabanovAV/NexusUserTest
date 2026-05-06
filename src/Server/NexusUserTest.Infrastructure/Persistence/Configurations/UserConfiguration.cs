b9aadca22b4ee93b212b70669317edb75a89681c
Кабанов Алексей <AVK.Kabanov@gmail.com>
2026-05-06 15:30:05 +0600

    Добавлена конфигурация полей данных пользователя в базу данных

---
+++ src/Server/NexusUserTest.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexusUserTest.Domain.Entities;
+
+namespace NexusUserTest.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Lastname)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Firstname)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Login)
+                .IsRequired();
+
+            builder.Property(x => x.Password)
+                .IsRequired();
+
+            builder.HasMany(s => s.GroupUsers)
+                .WithOne(g => g.User)
+                .HasForeignKey(g => g.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
